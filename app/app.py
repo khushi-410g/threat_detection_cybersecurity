445b79554e6fedb77f5afa42327aa972a6c06ee2
Khushi Goel <khushi.04g06@gmail.com>
2025-11-27 12:01:59 +0530

    making it real time

--- app/app.py
+++ app/app.py
@@ -14,25 +14,28 @@ def ip_hash(ip):
 @app.route("/detect", methods=["GET"])
 def detect():
     df = pd.read_csv("data/network_logs.csv")
-    row = df.tail(1).iloc[0]
 
-    X = [[
-        ip_hash(row["src_ip"]),
-        row["dst_port"],
-        row["packet_size"],
-        encoder.transform([row["protocol"]])[0]
-    ]]
+    # Get last 10 logs (or however many you want)
+    recent = df.tail(10)
 
-    pred = model.predict(X)[0]
-    conf = model.predict_proba(X).max()
+    results = []
 
-    return jsonify({
-        "threat": pred,
-        "confidence": float(conf),
-        "ip": row["src_ip"]
-    })
+    for _, row in recent.iterrows():
+        X = [[
+            ip_hash(row["src_ip"]),
+            row["dst_port"],
+            row["packet_size"],
+            encoder.transform([row["protocol"]])[0]
+        ]]
 
-if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
+        pred = model.predict(X)[0]
+        conf = model.predict_proba(X).max()
 
+        results.append({
+            "ip": row["src_ip"],
+            "threat": pred,
+            "confidence": float(conf),
+            "time": row["timestamp"]
+        })
 
+    return jsonify(results)
