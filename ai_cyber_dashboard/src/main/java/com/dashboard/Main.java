1186b1a5a222a2e6442ce2bb5f352b3567319c3e
Khushi Goel <khushi.04g06@gmail.com>
2025-11-28 12:34:10 +0530

    updates

--- ai_cyber_dashboard/src/main/java/com/dashboard/Main.java
+++ ai_cyber_dashboard/src/main/java/com/dashboard/Main.java
@@ -3,10 +3,20 @@ package com.dashboard;
 import com.formdev.flatlaf.FlatDarculaLaf;
 
 public class Main {
+
     public static void main(String[] args) {
+
         FlatDarculaLaf.setup();
 
+        //  Start Flask Backend Automatically
+        PythonServer server = new PythonServer();
+        server.startServer();
+
+        // Launch Dashboard UI
         DashboardUI ui = new DashboardUI();
         ui.setVisible(true);
+
+        // Stop Flask when UI closes
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stopServer));
     }
 }
