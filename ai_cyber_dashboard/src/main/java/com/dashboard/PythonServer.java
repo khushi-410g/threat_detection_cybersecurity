ce63230d2b649215995b4dbfd9c989f80a1afc2f
Khushi Goel <khushi.04g06@gmail.com>
2025-11-28 12:40:51 +0530

    final python server

--- ai_cyber_dashboard/src/main/java/com/dashboard/PythonServer.java
+++ ai_cyber_dashboard/src/main/java/com/dashboard/PythonServer.java
@@ -5,20 +5,17 @@ import java.io.File;
 public class PythonServer {
 
     private Process process;
+
     private final String projectPath =
             "/home/khushigoel/Desktop/Threat Detection Private Repo /threat_detection_cybersecurity";
 
     public void startServer() {
-
         try {
             System.out.println("Starting Flask backend...");
 
-            // Full path to venv python
-            String pythonPath = projectPath + "/.venv/bin/python3";
-
             ProcessBuilder pb = new ProcessBuilder(
-                    pythonPath,
-                    projectPath + "/app/app.py"
+                projectPath + "/.venv/bin/python3",
+                projectPath + "/app/app.py"
             );
 
             pb.directory(new File(projectPath));
@@ -27,11 +24,9 @@ public class PythonServer {
 
             process = pb.start();
 
-            // Wait for Flask server to start
             Thread.sleep(2000);
 
-            System.out.println("✅ Flask backend started successfully.");
-
+            System.out.println("✅ Flask backend started!");
         } catch (Exception e) {
             System.out.println("❌ Failed to start Flask backend: " + e.getMessage());
         }
