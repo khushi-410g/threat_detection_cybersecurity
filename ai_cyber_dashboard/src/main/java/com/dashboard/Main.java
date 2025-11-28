2c9b5381b9b2cb4c28063bbb07f10eaab746a3b1
Khushi Goel <khushi.04g06@gmail.com>
2025-11-28 12:27:41 +0530

    main java

--- ai_cyber_dashboard/src/main/java/com/dashboard/Main.java
+++ ai_cyber_dashboard/src/main/java/com/dashboard/Main.java
@@ -4,19 +4,9 @@ import com.formdev.flatlaf.FlatDarculaLaf;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Modern Dark Theme
         FlatDarculaLaf.setup();
 
-        // Auto-start Python backend
-        PythonServer server = new PythonServer();
-        server.startServer();
-
-        // Launch UI
         DashboardUI ui = new DashboardUI();
         ui.setVisible(true);
-
-        // Stop Python when closing app
-        Runtime.getRuntime().addShutdownHook(new Thread(server::stopServer));
     }
 }
